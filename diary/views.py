2613ac6476111ebcfe7b437420ccff2282a9e19c
hidekazuyamamoto <b.r.j.hide@gmail.com>
2021-09-18 09:05:00 +0900

    9/18

--- diary/views.py
+++ diary/views.py
@@ -1,10 +1,10 @@
 import logging
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import InquiryForm
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Diary
+from .forms import InquiryForm, DiaryCreateForm
 
 logger = logging.getLogger(__name__)
 
@@ -34,7 +34,25 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
 
-class DiaryDetailView(LoginRequiredMixin,generic.DetailView):
+
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
     model = Diary
     template_name = 'diary_detail.html'
-    
+
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:dairy_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, '日記の作成に失敗しました')
+        return super().form_invalid(form)
